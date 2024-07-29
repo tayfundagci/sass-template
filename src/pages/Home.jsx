04f65a056cf467dc60fb275ff32f6d36501ef287
tayfundagci <tayfun.dagci@brands-a.com>
2024-07-29 11:36:28 +0300

    initialize sass template

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className="container">
+        <span className='textOne'>SASS TRAINING</span>
+        <p className="textTwo"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro perferendis hic illo soluta! Sint vitae, fugiat molestias pariatur odit, facilis atque minima blanditiis maxime, facere et omnis reprehenderit! Quia, neque?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
